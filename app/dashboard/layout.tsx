c8cc776e847ffe682ab4ae62fd806b0c64e973b0
Salman Al Majali <undermod007@gmail.com>
2025-07-12 22:33:08 +0700

    Add layout page for /dashboard path

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,66 @@
+import { figtree } from '@/components/fonts'
+import { AppSidebar } from '@/components/ui/app-sidebar'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
+import { SiteHeader } from '@/components/ui/site-header'
+import { Breadcrumb } from '@/lib/definitions'
+import clsx from 'clsx'
+import Link from 'next/link'
+import React, { ReactNode } from 'react'
+
+function layout({ children }: { children: ReactNode }) {
+    return (
+        <SidebarProvider
+            style={
+                {
+                    "--sidebar-width": "calc(var(--spacing) * 72)",
+                    "--header-height": "calc(var(--spacing) * 12)",
+                } as React.CSSProperties
+            }
+        >
+            <AppSidebar variant='inset' />
+            <SidebarInset>
+                <SiteHeader />
+                <div className="flex flex-1 flex-col">
+                    <div className='@container/main flex flex-1 flex-col gap-2'>
+                        <div className="flex flex-col gap-4 p-4 md:gap-6 md:p-6">
+                            {children}
+                        </div>
+                    </div>
+                </div>
+            </SidebarInset>
+        </SidebarProvider>
+    )
+}
+
+export default layout
+
+export const PageTitle = ({ title }: { title: string }) => (
+    <h1 className={`${figtree.className} scroll-m-20 mb-4 text-2xl font-extrabold tracking-tight text-balance`}>
+        {title}
+    </h1>
+)
+
+export const Breadcrumbs = ({
+    breadcrumbs,
+}: {
+    breadcrumbs: Breadcrumb[];
+}) => (
+    <nav aria-label="Breadcrumb" className="mb-6 block">
+        <ol className={clsx(figtree.className, 'flex text-xl md:text-2xl font-extrabold tracking-tight text-balance')}>
+            {breadcrumbs.map((breadcrumb, index) => (
+                <li
+                    key={breadcrumb.href}
+                    aria-current={breadcrumb.active}
+                    className={clsx(
+                        breadcrumb.active ? 'text-black dark:text-white' : 'text-neutral-500',
+                    )}
+                >
+                    <Link href={breadcrumb.href}>{breadcrumb.label}</Link>
+                    {index < breadcrumbs.length - 1 ? (
+                        <span className="mx-3 inline-block">/</span>
+                    ) : null}
+                </li>
+            ))}
+        </ol>
+    </nav>
+)
