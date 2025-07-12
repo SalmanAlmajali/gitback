8884984bd76be79d9636f5d9678efffe624f4cd4
Salman Al Majali <undermod007@gmail.com>
2025-07-12 22:38:50 +0700

    Add letter nav main component

---
+++ components/ui/nav-main.tsx
@@ -0,0 +1,57 @@
+"use client"
+
+import { IconCirclePlusFilled, IconMail, type Icon } from "@tabler/icons-react"
+
+import { Button } from "@/components/ui/button"
+import {
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { usePathname } from "next/navigation"
+import clsx from "clsx"
+import Link from "next/link"
+
+export function NavMain({
+	items,
+}: {
+	items: {
+		title: string
+		url: string
+		icon?: Icon
+	}[]
+}) {
+	const pathname = usePathname();
+	return (
+		<SidebarGroup>
+			<SidebarGroupContent className="flex flex-col gap-2">
+				<SidebarMenu>
+					<SidebarMenuItem className="flex items-center gap-2">
+					</SidebarMenuItem>
+				</SidebarMenu>
+				<SidebarMenu>
+					{items.map((item) => (
+						<SidebarMenuItem key={item.title}>
+							<SidebarMenuButton
+								tooltip={item.title}
+								className={clsx('',
+									{
+										"bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear": pathname == item?.url
+									}
+								)}
+								asChild
+							>
+								<Link href={item?.url}>
+									{item.icon && <item.icon />}
+									<span>{item.title}</span>
+								</Link>
+							</SidebarMenuButton>
+						</SidebarMenuItem>
+					))}
+				</SidebarMenu>
+			</SidebarGroupContent>
+		</SidebarGroup>
+	)
+}
