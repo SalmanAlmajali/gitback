95c84a08013a2acd4bf653c8f761e0b5bb255ac1
Salman Al Majali <undermod007@gmail.com>
2025-07-12 22:41:30 +0700

    Delete moved fonts file

--- src/fonts.ts
+++
@@ -1,9 +0,0 @@
-import { Figtree, JetBrains_Mono } from "next/font/google";
-
-export const figtree = Figtree({
-  subsets: ['latin']
-});
-
-export const jetBrainsMono = JetBrains_Mono({
-  subsets: ['latin']
-});
