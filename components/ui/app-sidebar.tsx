cb70bffe2a56975456c8268beca5330328f35617
Salman Al Majali <undermod007@gmail.com>
2025-07-12 22:38:05 +0700

    Add letter app sidebar component

---
+++ components/ui/app-sidebar.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import * as React from "react"
+import {
+    IconBookmarks,
+    IconDashboard,
+    IconInnerShadowTop,
+} from "@tabler/icons-react"
+
+import { NavMain } from "@/components/ui/nav-main"
+import { NavUser } from "@/components/ui/nav-user"
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { figtree } from "../fonts"
+import Link from "next/link"
+
+const data = {
+    user: {
+        name: "shadcn",
+        email: "m@example.com",
+        avatar: "/avatars/shadcn.jpg",
+    },
+    navMain: [
+        {
+            title: "Dashboard",
+            url: "/dashboard",
+            icon: IconDashboard,
+        },
+        {
+            title: "Repositories",
+            url: "/dashboard/repositories",
+            icon: IconBookmarks,
+        },
+    ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+    return (
+        <Sidebar collapsible="offcanvas" {...props}>
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton
+                            asChild
+                            className="data-[slot=sidebar-menu-button]:!p-1.5"
+                        >
+                            <Link href={'/'} className={figtree.className}>
+                                <IconInnerShadowTop className="!size-5" />
+                                <span className="text-base font-semibold">Gitback</span>
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+            <SidebarContent>
+                <NavMain items={data.navMain} />
+            </SidebarContent>
+            <SidebarFooter>
+                <NavUser user={data.user} />
+            </SidebarFooter>
+        </Sidebar>
+    )
+}
