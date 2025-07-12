d467fe527a16ac5b6fce9ef414daeebb66d2e980
Salman Al Majali <undermod007@gmail.com>
2025-07-12 22:34:19 +0700

    Add edit repository page

---
+++ app/dashboard/repositories/[id]/edit/page.tsx
@@ -0,0 +1,43 @@
+import { Breadcrumbs, PageTitle } from '@/app/dashboard/layout'
+import { fetchRepositoryById } from '@/lib/data'
+import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
+import React from 'react'
+
+export const metadata: Metadata = {
+	title: "Edit Repository"
+}
+
+async function Page(props: { params: Promise<{ id: string }> }) {
+	const params = await props.params;
+	const id = params.id;
+
+	const repository = await fetchRepositoryById(id);
+
+	if (!repository) {
+		notFound();
+	}
+
+	return (
+		<div>
+			<Breadcrumbs
+				breadcrumbs={[
+					{ label: 'Repositories', href: '/dashboard/repositories' },
+					{
+						label: 'Edit Repository',
+						href: `/dashboard/repositories/${id}/edit`,
+						active: true,
+					},
+				]}
+			/>
+			<p>Datanya ada di database. Males bikin formnya jadi, gini aja dulu</p>
+			<pre className="bg-neutral-100 dark:bg-neutral-900 p-4 rounded-md overflow-x-auto">
+				<code className="font-mono">
+					{JSON.stringify(repository, null, 2)}
+				</code>
+			</pre>
+		</div>
+	)
+}
+
+export default Page
