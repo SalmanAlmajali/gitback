dd125ac0041c2e1de76718e08080bd50e4fadcc6
Salman Al Majali <undermod007@gmail.com>
2025-07-12 22:32:37 +0700

    Add login page

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,26 @@
+import { LoginForm } from "@/components/ui/login-form"
+import LetterGlitch from "@/components/blocks/Backgrounds/LetterGlitch/LetterGlitch"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Login'
+}
+
+export default function Page() {
+    return (
+        <>
+            <LetterGlitch
+                glitchSpeed={0}
+                centerVignette={false}
+                outerVignette={true}
+                smooth={true}
+                glitchColors={["#61cf5a", "#63ad58", "#50864c", "#3e6a3d", "#3b4b33"]}
+            />
+            <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+                <div className="w-full max-w-sm">
+                    <LoginForm />
+                </div>
+            </div>
+        </>
+    )
+}
