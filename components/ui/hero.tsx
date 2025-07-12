43b4704818f347a849d7f096d57e2696825b61fa
Salman Al Majali <undermod007@gmail.com>
2025-07-12 22:38:30 +0700

    Add letter hero component

---
+++ components/ui/hero.tsx
@@ -0,0 +1,14 @@
+export default function Hero() {
+    return (
+        <section
+            id="top"
+            className="py-32 px-6 text-center bg-neutral-200 dark:bg-neutral-900"
+        >
+            <h1 className="text-5xl font-bold mb-4">Gitback</h1>
+            <p className="text-lg max-w-2xl mx-auto">
+                Platform sederhana untuk menerima dan mengelola feedback pengguna,
+                lalu mengubahnya menjadi issue di GitHub secara otomatis.
+            </p>
+        </section>
+    )
+}
