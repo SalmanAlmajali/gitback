ccb426984c915ed9170c7933bf6f810cc862c0ab
Salman Al Majali <undermod007@gmail.com>
2025-07-18 09:17:09 +0700

    Refactor Table component to use renderCell function for dynamic cell rendering and improve type consistency

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -3,24 +3,23 @@ import Link from 'next/link';
 import { Row, RowList } from 'postgres';
 import { figtree } from '../fonts';
 import { Separator } from './separator';
-import { RepositoriesTable } from '@/app/lib/definitions';
+import { RepositoriesTable, RepositoriesTableRow } from '@/app/lib/repositories/definitions';
+import { RenderCellFunction, TableHeadColumn } from '@/app/lib/definitions';
 
 export default async function Table({
     pageName,
     query,
     currentPage,
     tableHead,
+    renderCell,
     fetchFilteredFunction,
 }: {
     pageName: string;
     query: string;
     currentPage: number;
-    tableHead: {
-        label: string,
-        key: string,
-        type: string,
-    }[];
-    fetchFilteredFunction: (query: string, currentPage: number) => Promise<RowList<RepositoriesTable[]>> | Promise<RowList<Row[]>> | Promise<RepositoriesTable[]>;
+    tableHead: TableHeadColumn[];
+    renderCell: RenderCellFunction<RepositoriesTableRow>
+    fetchFilteredFunction: (query: string, currentPage: number) => Promise<RepositoriesTableRow[]>;
 }) {
     const datas = await fetchFilteredFunction(query, currentPage);
 
@@ -28,7 +27,7 @@ export default async function Table({
 
 
     return (
-        <div className="mt-6 flow-root">
+        <div className="mt-6 flow-root overflow-x-scroll">
             <div className="inline-block min-w-full align-middle">
                 <div className="rounded-lg bg-neutral-100 dark:bg-neutral-900 p-2 md:pt-0">
                     <div className="md:hidden">
@@ -46,20 +45,22 @@ export default async function Table({
                                                     orientation="vertical"
                                                     className="mx-2 data-[orientation=vertical]:h-4"
                                                 />
-                                                <p>{repository.name}</p>
+                                                <p>
+                                                    {renderCell(repository, tableHead[0])}
+                                                </p>
                                             </div>
-                                            <p className={`${figtree.className} text-sm text-gray-500`}>{repository.username}</p>
-                                            <p className={`${figtree.className} text-sm text-gray-500`}>{repository.email}</p>
+                                            <p className={`${figtree.className} text-sm text-gray-500`}>{renderCell(repository, tableHead[3])}</p>
+                                            <p className={`${figtree.className} text-sm text-gray-500`}>{renderCell(repository, tableHead[4])}</p>
                                         </div>
                                     </div>
                                     <div className="flex flex-col w-full items-start justify-between gap-y-4 pt-4">
                                         <div className='space-y-2'>
                                             <small className={figtree.className}>Github Owner:</small>
                                             <p className="text-xl font-medium">
-                                                {repository.github_owner}
+                                                {renderCell(repository, tableHead[1])}
                                             </p>
                                             <small className={figtree.className}>Github Repository:</small>
-                                            <p className="text-xl font-medium">{repository.github_repo}</p>
+                                            <p className="text-xl font-medium">{renderCell(repository, tableHead[2])}</p>
                                         </div>
                                         <div className="flex justify-end gap-2">
                                             <Update id={repository.id} pageName={pageName} />
@@ -107,14 +108,14 @@ export default async function Table({
                                                     case 'date':
                                                         return (
                                                             <td className="whitespace-nowrap px-3 py-3" key={td?.key}>
-                                                                {new Date((tr as Record<string, string>)?.[td.key]).toLocaleDateString()}
+                                                                {renderCell(tr, td)}
                                                             </td>
                                                         )
 
                                                     default:
                                                         return (
                                                             <td className="whitespace-nowrap px-3 py-3" key={td?.key}>
-                                                                {(tr as Record<string, string>)?.[td.key]}
+                                                                {renderCell(tr, td)}
                                                             </td>
                                                         )
                                                 }
