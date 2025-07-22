fe54ba42b5884f309c0fa32d66ebfe755ca1f10d
Salman Al Majali <undermod007@gmail.com>
2025-07-22 21:54:31 +0700

    Add function to fetch authenticated user repositories from GitHub API

---
+++ app/lib/github/api.ts
@@ -0,0 +1,40 @@
+import { GitHubRepoApiData } from "../repositories/definitions";
+
+export async function getAuthenticatedUserRepos(accessToken: string | undefined): Promise<GitHubRepoApiData[]> {
+    if (!accessToken) {
+        throw new Error('GitHub access token is missing.');
+    }
+
+    let allRepos: GitHubRepoApiData[] = [];
+    let page = 1;
+    const per_page = 100;
+
+    while (true) {
+        const response = await fetch(`https://api.github.com/user/repos?per_page=${per_page}&page=${page}`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                Accept: 'application/vnd.github+json',
+            },
+            cache: 'no-store',
+        });
+
+        if (!response.ok) {
+            const errorText = await response.text();
+            console.error(`GitHub API error (status: ${response.status}):`, errorText);
+            throw new Error(`Failed to fetch GitHub repositories: ${response.statusText}. Details: ${errorText}`);
+        }
+
+        const repos: GitHubRepoApiData[] = await response.json();
+        allRepos = allRepos.concat(repos);
+
+        const linkHeader = response.headers.get('Link');
+        const hasNextPage = linkHeader && linkHeader?.includes('rel="next"');
+
+        if (!hasNextPage || repos.length < per_page) {
+            break;
+        }
+        page++
+    }
+
+    return allRepos;
+}
