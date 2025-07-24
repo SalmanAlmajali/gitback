1280107335cdce3ef527c160c09f74753e9bcc4e
Salman Al Majali <undermod007@gmail.com>
2025-07-24 11:43:13 +0700

    Refactor removing Toaster component and moving it to RooTLayout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,7 +6,6 @@ import { Breadcrumb } from '@/app/lib/definitions'
 import clsx from 'clsx'
 import Link from 'next/link'
 import React, { ReactNode } from 'react'
-import { Toaster } from 'sonner'
 
 function layout({ children }: { children: ReactNode }) {
     return (
@@ -25,7 +24,6 @@ function layout({ children }: { children: ReactNode }) {
                     <div className='@container/main flex flex-1 flex-col gap-2'>
                         <div className="flex flex-col gap-4 p-4 md:gap-6 md:p-6">
                             {children}
-                            <Toaster />
                         </div>
                     </div>
                 </div>
