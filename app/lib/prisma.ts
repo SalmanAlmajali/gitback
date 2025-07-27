00a3cf59127734596ca04632ece8235a2ec4157c
Salman Al Majali <undermod007@gmail.com>
2025-07-27 18:35:46 +0700

    Refactor new prisma configuration

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,25 +1,7 @@
-// app/lib/prisma.ts
-
-import { PrismaClient } from '@prisma/client';
-
-// Declare a global variable for PrismaClient
-// This is necessary to prevent multiple instances of PrismaClient in development
-// (due to Next.js hot-reloading)
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
-
-// Initialize the PrismaClient instance
-// In development, reuse the existing global instance if it exists.
-// In production, create a new instance.
-const prisma = global.prisma || new PrismaClient();
-
-// In development, assign the PrismaClient instance to the global variable
-// so it's reused across hot reloads.
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma;
-}
-
-// Export the PrismaClient instance
-export default prisma;
+import { PrismaClient } from "@prisma/client"
+ 
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+ 
+export const prisma = globalForPrisma.prisma || new PrismaClient()
+ 
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
