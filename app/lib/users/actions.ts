9af86f3583452740c1c097c7397b7dff7c15d1c0
Salman Al Majali <undermod007@gmail.com>
2025-07-27 18:37:40 +0700

    Refactor: handle login action from server

--- app/lib/users/actions.ts
+++ app/lib/users/actions.ts
@@ -2,14 +2,35 @@
 
 import bcrypt from "bcryptjs";
 import * as z from "zod";
-import prisma from "@/app/lib/prisma";
 import { CustomResponse } from "../definitions";
+import { prisma } from "../prisma";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 const SignUpUserFormSchema = z.object({
     name: z.string().min(1, "Name is required."),
     email: z.string().email("Invalid email address."),
     password: z.string().min(6, "Password must be at least 6 characters."),
 });
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
+}
 
 export async function createUser(formData: FormData): Promise<CustomResponse> {
     const parsed = SignUpUserFormSchema.safeParse({
@@ -43,8 +64,21 @@ export async function createUser(formData: FormData): Promise<CustomResponse> {
             },
         });
 
+        await signIn('credentials', {
+            redirect: false,
+            ...Object.fromEntries(formData),
+        });
+
         return { success: true, message: "You're all set! Your account is ready to use." };
     } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return { success: false, error: 'Invalid credentials.' };
+                default:
+                    return { success: false, error: 'Something went wrong.' };
+            }
+        }
         console.error("Server-side signup error:", error);
         return { success: false, error: 'Failed to register user. Please try again.' };
     }
