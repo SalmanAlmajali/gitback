86622a3a8a2e3d8e747ce170894abfc0fc801747
Salman Al Majali <undermod007@gmail.com>
2025-08-01 16:54:46 +0700

    fix: enhance form resposivity

--- components/ui/repositories/edit-form.tsx
+++ components/ui/repositories/edit-form.tsx
@@ -3,7 +3,7 @@
 import { IconClock, IconCode, IconDeviceFloppy, IconEyeCheck, IconEyeExclamation, IconGitBranch, IconGitFork, IconLink, IconLoader2, IconRefresh, IconStar, IconTextCaption } from '@tabler/icons-react';
 import { Button } from '../button';
 import { UserSelectedRepository } from '@prisma/client';
-import { Card, CardAction, CardContent, CardDescription, CardHeader, CardTitle } from '../card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../card';
 import { figtree } from '@/components/fonts';
 import MyInput, { MyTextArea } from '../my-input';
 import { useState } from 'react';
@@ -109,32 +109,6 @@ export default function EditForm({
             <CardHeader>
                 <CardTitle className={`${figtree.className} text-2xl font-bold leading-tight`}>Update Repository</CardTitle>
                 <CardDescription>If U are logged in using GitHub account or already linked your account, U can automaticaly sync your repository directly from GitHub or edit manually.</CardDescription>
-                <CardAction>
-                    {session.data?.accessToken !== undefined ? (
-                        <Button
-                            onClick={handleSync}
-                            disabled={loading.sync}
-                        >
-                            {loading.sync ? (
-                                <>
-                                    <IconRefresh className="mr-2 h-4 w-4 animate-spin" /> Sync...
-                                </>
-                            ) : (
-                                <>
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                        <path
-                                            d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
-                                            fill="currentColor"
-                                        />
-                                    </svg>
-                                    Sync with GitHub repository
-                                </>
-                            )}
-                        </Button>
-                    ) : (
-                        <ConnectGitHub />
-                    )}
-                </CardAction>
             </CardHeader>
             <CardContent>
                 <form action={updateRepositoryWithId}>
@@ -192,7 +166,7 @@ export default function EditForm({
                             Repository Visibility
                         </legend>
                         <div className="rounded-md border px-[14px] py-3">
-                            <div className="flex gap-4">
+                            <div className="flex flex-wrap gap-4">
                                 <div className="flex items-center">
                                     <input
                                         id="public"
@@ -279,26 +253,52 @@ export default function EditForm({
                             defaultValue={repository.updatedAt.toISOString().split('Z', 1)[0]}
                         />
                     </div>
-                    <div className='flex justify-end gap-x-2'>
-                        <LinkButton
-                            href="/dashboard/repositories"
-                            className='px-4 rounded-lg'
-                            variant='secondary'
-                        >
-                            Cancel
-                        </LinkButton>
-                        <Button type='submit' disabled={loading.update}>
-                            {loading.update ? (
-                                <>
-                                    <IconLoader2 className="mr-2 h-4 w-4 animate-spin" /> Updating...
-                                </>
-                            ) : (
-                                <>
-                                    <IconDeviceFloppy />
-                                    Update Repository
-                                </>
-                            )}
-                        </Button>
+                    <div className='flex flex-col-reverse justify-between w-full mt-4 gap-2'>
+                        <div className='flex flex-col-reverse md:flex-row md:justify-end gap-2'>
+                            <LinkButton
+                                href="/dashboard/repositories"
+                                className='px-4 py-2 rounded-lg'
+                                variant='secondary'
+                            >
+                                Cancel
+                            </LinkButton>
+                            <Button type='submit' disabled={loading.update}>
+                                {loading.update ? (
+                                    <>
+                                        <IconLoader2 className="mr-2 h-4 w-4 animate-spin" /> Updating...
+                                    </>
+                                ) : (
+                                    <>
+                                        <IconDeviceFloppy />
+                                        Update Repository
+                                    </>
+                                )}
+                            </Button>
+                        </div>
+                        {session.data?.accessToken !== undefined ? (
+                            <Button
+                                onClick={handleSync}
+                                disabled={loading.sync}
+                            >
+                                {loading.sync ? (
+                                    <>
+                                        <IconRefresh className="mr-2 h-4 w-4 animate-spin" /> Sync...
+                                    </>
+                                ) : (
+                                    <>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                            <path
+                                                d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+                                                fill="currentColor"
+                                            />
+                                        </svg>
+                                        Sync with GitHub
+                                    </>
+                                )}
+                            </Button>
+                        ) : (
+                            <ConnectGitHub />
+                        )}
                     </div>
                 </form>
             </CardContent>
